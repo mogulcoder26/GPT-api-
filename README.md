004323e451e384e97c999874c2186031801ff8f3
Soubhik <b422056@iiit-bh.ac.in>
2023-04-12 20:53:08 +0530

    youtube search api setup

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GPT-api-
 repo for generating prompts and using runCompletions from GPT api
+
+# Axios:
+
+axios is a library that provides a simple and easy-to-use interface for making HTTP requests in Node.js. In this particular case, it is used to make a GET request to the YouTube Search Results API to retrieve data in JSON format.
+
+While you could use other libraries or built-in Node.js modules such as http, https, or request to make HTTP requests, axios provides a number of advantages, including:
+
+A simple and easy-to-use API with methods for all HTTP request types (GET, POST, PUT, DELETE, etc.)
+Automatic handling of JSON data, including parsing and stringifying
+Automatic handling of response codes and errors
+Support for request and response interceptors to modify requests and responses
+Support for canceling requests and timeouts
+Browser and Node.js compatibility
+Using axios can simplify the process of making HTTP requests in Node.js, and provide a more consistent and reliable experience for handling responses and errors.
---
+++ youtube search api/app.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://youtube-search-results.p.rapidapi.com/youtube-search/',
+        params: { q: 'justinbieber' },
+        headers: {
+            'X-RapidAPI-Key': '95992756cbmshdd903a59b07377ap1b3e73jsn36c4b8682a30',
+            'X-RapidAPI-Host': 'youtube-search-results.p.rapidapi.com'
+        }
+    };
+
+    axios.request(options)
+        .then((response) => {
+            console.log(response.data);
+            const data = (response.data);
+            return data;
+        }).then((data) => {
+            console.log(data.items);
+            let items = data.items;
+            // res.json(items);
+            return items;
+        }).then((items) => {
+            let array=[];
+            items.forEach((video) => {
+                let obj={
+                    title:video.title,
+                    url:video.url
+                }
+                array.push(obj);
+            });
+            // res.json(array);
+            return array;
+        }).then((array)=>{
+            let finalArr=[];
+            array.forEach((e)=>{
+                finalArr.push(e)
+            })
+            res.json(finalArr);//can extract for further use!!!
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+    // axios.request(options)
+    // .then(function (response) {
+    //     console.log(response.data);
+    //     res.json(response.data.items);
+    // }).catch(function (error) {
+    //     console.error(error);
+    //     res.status(500).send('Internal server error');
+    // });
+
+})
+
+app.listen(3000, () => {
+    console.log('Server Live @ 3000')
+})
